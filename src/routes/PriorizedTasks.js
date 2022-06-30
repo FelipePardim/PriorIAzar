044c9e9093188cc51f2c249a13a891d8583ec61a
Felipe Pardim <felipesantos.2017@alunos.utfpr.edu.br>
2022-06-30 19:30:14 -0300

    Adding  API fetch

--- src/routes/PriorizedTasks.js
+++ src/routes/PriorizedTasks.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 import './index.css';
 
+const baseURL = "https://localhost:3500";
+
 export default function PriorizedTasks() {
+
+  const [DNA, setDNA] = useState('');
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setDNA(response.data);
+      console.log(DNA);
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -14,6 +26,7 @@ export default function PriorizedTasks() {
           <tbody>
             <tr>DNA:
               <td>MOCK1</td>
+              <td>{DNA}</td>
             </tr>
             <tr>Fitness:
               <td>MOCK1</td>
